2daa25d3abe144d27c6f50a0e01b0e9bee156b13
Karl Macadangdang <karljarren0308@yahoo.com>
2018-06-06 13:01:54 +0800

    New Update(s)

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->prefix('api/json')->group(function() {
+    Route::post('auth', 'ApiController@auth');
 });
