38c2c2a0e282bcf386fee72b9998949f492d8537
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-07-22 03:30:18 +0800

    Added View Info button

--- resources/views/partials/table_candidates.blade.php
+++ resources/views/partials/table_candidates.blade.php
@@ -4,6 +4,7 @@
     <td>{{ $candidate->party_info->name }}</td>
     <td>{{ $candidate->position_info->name }}</td>
     <td class="text-center">
+        <button class="button info small"><span class="fas fa-bars"></span></button>
         <button class="button success small"><span class="fas fa-edit"></span></button>
         <button class="button danger small"><span class="fas fa-trash-alt"></span></button>
     </td>
