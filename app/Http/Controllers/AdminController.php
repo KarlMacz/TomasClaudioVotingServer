e0b58400f7db3a5aa205841966f18a5ad6fe520f
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-07-25 02:20:00 +0800

    Increased number of candidates to display on table from 2 to 10

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -33,7 +33,7 @@ class AdminController extends Controller
 
     public function candidates()
     {
-        $candidates = Candidates::paginate(2);
+        $candidates = Candidates::paginate(10);
 
         return view('admin.candidates', [
             'candidates' => $candidates
